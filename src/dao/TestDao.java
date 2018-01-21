289bf85519b33e054ba47c0cfeef65c5a3328c2c
lingxiezfy <1241948182@qq.com>
2018-01-21 18:26:19 +0800

    添加注释示例

--- src/dao/TestDao.java
+++ src/dao/TestDao.java
@@ -1,4 +1,18 @@
 package dao;
 
+import vo.Test;
+
+import java.util.List;
+
 public class TestDao {
+
+    /**
+     * 通过id查询test数据
+     * @param id 需要查询的id
+     * @return 成功查询返回Test对象，失败返回null
+     */
+    public Test getTestById(int id){
+        Test test  = null;
+        return test;
+    }
 }
